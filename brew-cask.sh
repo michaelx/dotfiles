9d28fab17793a7d00acf086a07d95dc5dde49440
Michael Xander <michimself@gmail.com>
2016-10-15 18:39:25 +0200

    Updated my macOS config, added my brew and brew-cask, added my ST settings

---
+++ brew-cask.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# To maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+# Archiving
+brew cask install keka
+brew cask install the-unarchiver
+
+# Audio
+brew cask install amazon-music
+brew cask install music-manager
+brew cask install spotify
+brew cask install spotifree
+brew cask install max
+brew cask install radiant-player
+
+# Backup & Clone and Synchronization
+brew cask install arrsync
+brew cask install dropbox
+brew cask install arq
+
+# Burning & Ripping
+brew cask install burn
+brew cask install handbrake
+brew cask install makemkv
+
+# Development
+brew cask install sublime-text
+brew cask install cyberduck
+brew cask install forklift
+brew cask install sourcetree
+brew cask install sequel-pro
+brew cask install colorpicker-developer
+brew cask install colorpicker-hex
+brew cask install screaming-frog-seo-spider
+brew cask install integrity
+
+# Energy
+brew cask install coconutbattery
+
+# Image Editing
+brew cask install acorn
+brew cask install imageoptim
+brew cask install imagealpha
+
+# Internet
+brew cask install google-chrome
+brew cask install firefox
+brew cask install skype
+brew cask install transmission
+brew cask install paparazzi
+brew cask install tunnelbear
+brew cask install tunnelblick
+brew cask install fluid
+brew cask install sitesucker
+
+# Monitoring
+brew cask install drobo-dashboard
+
+# Office
+brew cask install macdown
+brew cask install kindle
+brew cask install licecap
+
+# Productivity
+brew cask install slack
+brew cask install notational-velocity
+brew cask install 1password
+brew cask install textexpander
+brew cask install mattr-slate
+brew cask install flux
+
+# Renaming
+brew cask install forklift
+
+# Services & Scripts
+brew cask install omnidisksweeper
+
+# (UI) Design
+brew cask install sketch
+brew cask install glueprint
+brew cask install ember
+
+# Sketch Utilities
+brew cask install sketch-toolbox
+
+# Video
+brew cask install vlc
+
+# Virtualize
+brew cask install virtualbox
+
+
+# Not on cask but I want regardless
+
+# Illustrator
+# Photoshop
+# Xcode
+# Amphetamine
+# ReadKit
+# Tweetbot
+# Express Talk VoIP
+# Terminology for OS X Dictionary
+# OpenThesaurus Deutsch
+# BeoLingus Deutsch-Englisch
+# Marked
+# Calca
+# Fantastical
+# Scapple
+# Notational Velocity
+# Hazel
+# tvnamer
