51dbd96ac438fefb389ad522f95798d378f923e2
Michael Xander <michimself@gmail.com>
2020-11-13 17:24:36 +0100

    Update Hammerspoon config
    - Add `toggle_input_devices` to connect/disconnect bluetooth input devices (e.g. keyboard, mouse). Useful for shared usage between multiple devices.
    - Add launch app on `toggle_application`.
    - Update window-pos key-mappings (use VIM navigation).
    - Update application key-mappings.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,7 @@
 -- Default options
-hs.window.animationDuration = 0.1
+hs.window.animationDuration = 0.05
 
--- Define some keyboard modifier variables
+-- Keyboard modifiers
 hyper = {'ctrl', 'alt', 'cmd'}
 
 -- Watchers
@@ -15,9 +15,9 @@ hs.loadSpoon('MX')
 spoon.MX:bindHotkeys({toggle={hyper, 'c'}})
 spoon.MX:start()
 
--- Define monitor names for layout purposes
+-- Available monitors
 display_mbp = 'Color LCD'
-display_monitor = 'PL2492H'
+display_external_1 = 'PL2492H'
 
 -- Defines for screen watcher
 lastNumberOfScreens = #hs.screen.allScreens()
@@ -34,7 +34,7 @@ hs.grid.MARGINY = 0
 -- Defines for window maximize toggler
 frameCache = {}
 
--- Define window layouts
+-- Window layouts
 --   {'App name', 'Window name', 'Display Name', 'unitrect', 'framerect', 'fullframerect'},
 internal_display = {
   {'Bitwarden',               nil,          display_mbp, hs.layout.maximized, nil, nil},
@@ -55,6 +55,7 @@ internal_display = {
   {'Sketch',                  nil,          display_mbp, hs.layout.maximized, nil, nil},
   {'Sourcetree',              nil,          display_mbp, hs.layout.maximized, nil, nil},
   {'Sublime Text',            nil,          display_mbp, hs.layout.maximized, nil, nil},
+  {'TIDAL',                   nil,          display_mbp, hs.layout.maximized, nil, nil},
 }
 
 dual_display = {
@@ -67,22 +68,23 @@ dual_display = {
   {'Finder',                  nil,          display_mbp, hs.geometry.unitrect(2/10, 2/10, 6/10, 6/10), nil, nil},
   {'ForkLift',                nil,          display_mbp, hs.layout.maximized, nil, nil},
   {'iTerm2',                  nil,          display_mbp, hs.layout.maximized, nil, nil},
-  {'iTunes',                  'iTunes',     display_monitor, hs.layout.maximized, nil, nil},
+  {'iTunes',                  'iTunes',     display_external_1, hs.layout.maximized, nil, nil},
   {'MacDown',                 nil,          display_mbp, hs.geometry.unitrect(1/10, 1/10, 8/10, 8/10), nil, nil},
   {'Mail',                    nil,          display_mbp, hs.layout.maximized, nil, nil},
-  {'Messages',                nil,          display_monitor, hs.geometry.unitrect(2/10, 1/10, 6/10, 8/10), nil, nil},
+  {'Messages',                nil,          display_external_1, hs.geometry.unitrect(2/10, 1/10, 6/10, 8/10), nil, nil},
   {'Reeder',                  nil,          display_mbp, hs.layout.maximized, nil, nil},
   {'Safari',                  nil,          display_mbp, hs.layout.maximized, nil, nil},
-  {'Sketch',                  nil,          display_monitor, hs.layout.maximized, nil, nil},
+  {'Sketch',                  nil,          display_external_1, hs.layout.maximized, nil, nil},
   {'Sourcetree',              nil,          display_mbp, hs.layout.maximized, nil, nil},
-  {'Sublime Text',            nil,          display_monitor, hs.layout.maximized, nil, nil},
+  {'Sublime Text',            nil,          display_external_1, hs.layout.maximized, nil, nil},
+  {'TIDAL',                   nil,          display_mbp, hs.layout.maximized, nil, nil},
 }
 
 -- Toggle an application between being the frontmost app, and being hidden
 function toggle_application(_app)
   local app = hs.appfinder.appFromName(_app)
   if not app then
-    -- FIXME: This should really launch _app
+    hs.application.launchOrFocus(_app)
     return
   end
 
@@ -108,6 +110,14 @@ function toggle_window_maximized()
     frameCache[win:id()] = win:frame()
     win:maximize()
   end
+end
+
+-- Connect/disconnect bluetooth keyboard and mouse
+--   Uses https://formulae.brew.sh/formula/bluetoothconnector
+--   Replace device MAC addresses
+function toggle_input_devices()
+  hs.execute("BluetoothConnector 00-00-00-00-00-00", true)
+  hs.execute("BluetoothConnector 00-00-00-00-00-00", true)
 end
 
 -- Callback function for application events
@@ -147,7 +157,7 @@ function caffeinateCallback(eventType)
     local output = hs.audiodevice.defaultOutputDevice()
     shouldUnmuteOnScreenWake = not output:muted()
     output:setMuted(true)
-  
+
   elseif (eventType == hs.caffeinate.watcher.screensDidWake) then
     print('screensDidWake')
     if shouldUnmuteOnScreenWake then
@@ -163,10 +173,10 @@ hyperfns = {}
 hyperfns['t'] = toggle_window_maximized
 hyperfns['f'] = function() hs.window.focusedWindow():maximize() end
 hyperfns['r'] = function() hs.window.focusedWindow():toggleFullScreen() end
-hyperfns['['] = function() hs.window.focusedWindow():moveToUnit(hs.layout.left50) end
-hyperfns[']'] = function() hs.window.focusedWindow():moveToUnit(hs.layout.right50) end
-hyperfns['o'] = function() hs.window.focusedWindow():moveToUnit(hs.layout.left30) end
-hyperfns['p'] = function() hs.window.focusedWindow():moveToUnit(hs.layout.right30) end
+hyperfns['h'] = function() hs.window.focusedWindow():moveToUnit(hs.layout.left50) end
+hyperfns['j'] = function() hs.layout.apply({{nil, hs.window.focusedWindow(), hs.screen.mainScreen(), hs.geometry.unitrect(0, 0.5, 1, 0.5), nil, nil}}) end
+hyperfns['k'] = function() hs.layout.apply({{nil, hs.window.focusedWindow(), hs.screen.mainScreen(), hs.geometry.unitrect(0, 0, 1, 0.5), nil, nil}}) end
+hyperfns['l'] = function() hs.window.focusedWindow():moveToUnit(hs.layout.right50) end
 
 -- Hotkeys to trigger defined layouts
 hyperfns['1'] = function() hs.layout.apply(internal_display) end
@@ -180,7 +190,7 @@ hyperfns['Up'] = hs.grid.pushWindowUp
 hyperfns['Down'] = hs.grid.pushWindowDown
 
 -- Application hotkeys
-hyperfns['i'] = function() toggle_application('iTerm2') end
+hyperfns['`'] = function() toggle_application('iTerm2') end
 hyperfns['s'] = function() toggle_application('Safari') end
 
 -- Hotkey to display window hints
@@ -190,12 +200,15 @@ hyperfns['a'] = hs.hints.windowHints
 hyperfns['q'] = function() hs.reload() end
 hs.alert.show('Config loaded')
 
+-- Hotkey to connect/disconnect bluetooth input devices
+hyperfns['b'] = function() toggle_input_devices() end
+
 --- Bind all hotkeys
 for _hotkey, _fn in pairs(hyperfns) do
   hs.hotkey.bind(hyper, _hotkey, _fn)
 end
 
--- Create and start our callbacks
+-- Create and start callbacks
 appWatcher = hs.application.watcher.new(applicationWatcher):start()
 
 -- Note: I rarely use my dual screen setup these days, so auto-switching is off
